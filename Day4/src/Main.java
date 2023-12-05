48325dc4f0e697fea6f625fedf29ef27e10b118b
Jakub <k.nietupski99@gmail.com>
2023-12-05 16:36:10 +0100

    Day four

--- Day4/src/Main.java
+++ Day4/src/Main.java
@@ -1,20 +1,33 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
+    public static Map<Integer, Integer> map = new HashMap<>();
     public static void main(String[] args) {
         List<String> cards = Reader.readFromFile();
-        int result = 0;
-        for (String s : cards) {
-            String[] nums = s.split(":");
-            List<Integer> myNumbers = stringArrToIntArray(nums[1].split("\\|")[0]);
-            List<Integer> winningNumbers = stringArrToIntArray(nums[1].split("\\|")[1]);
-            result += part1(myNumbers, winningNumbers);
+        int result1 = 0;
+        int result2 = 0;
+
+        for (int i = 0; i < cards.size(); i++) {
+            map.put(i,1);
         }
 
-        System.out.println(result);
-    }
+        for (int i = 0; i < cards.size(); i++) {
+            String[] nums = cards.get(i).split(":");
+            List<Integer> myNumbers = stringArrToIntArray(nums[1].split("\\|")[0]);
+            List<Integer> winningNumbers = stringArrToIntArray(nums[1].split("\\|")[1]);
+            result1 += part1(myNumbers, winningNumbers, i);
+        }
 
+        for (int value : map.values()) {
+            result2 += value;
+        }
+
+        System.out.println("Part 1: " + result1);
+        System.out.println("Part 2: " + result2);
+    }
 
     private static List<Integer> stringArrToIntArray(String string) {
         StringBuilder stringBuffer = new StringBuilder();
@@ -37,20 +50,22 @@ public class Main {
         return numbers;
     }
 
-    private static int part1(List<Integer> myNumbers, List<Integer> winningNumbers) {
+    private static int part1(List<Integer> myNumbers, List<Integer> winningNumbers, int i) {
         int matches = 0;
-        int result = 0;
-        for (int i = 0; i < myNumbers.size(); i++) {
-            if (winningNumbers.contains(myNumbers.get(i))) {
+        int result1 = 0;
+        for (Integer winningNumber : winningNumbers) {
+            if (myNumbers.contains(winningNumber)) {
                 matches++;
                 if (matches == 1) {
-                    result += 1;
+                    result1 += 1;
                 } else {
-                    result *= 2;
+                    result1 *= 2;
                 }
             }
-
         }
-        return result;
+        for (int j = i + 1; j < i + matches + 1; j++) {
+            map.put(j, map.getOrDefault(i, 0) + map.getOrDefault(j,0));
+        }
+        return result1;
     }
 }
